2b7f47c5c997c82a755b67330aaf6ae8ddc5b09d
HelpPal <justinjones201811@gmail.com>
2018-06-28 23:54:49 +0800

    merge flask

---
+++ manage_merge.py
@@ -0,0 +1,25 @@
+Django==1.9.9
+djangorestframework==3.3.3
+python-dateutil==2.4.2
+django-assets==0.12
+django-countries==4.5
+# The four following are required filters that do not get installed
+# as prerequisites for django-assets
+jsmin==2.1.0
+cssmin==0.2.0
+yuicompressor==2.4.8
+django-crispy-forms==1.6.0
+django-extra-views==0.7.1
+pytz==2015.6
+
+#djaodjin-extended-templates==0.1.5
+#djaodjin-deployutils==0.4.0
+#djaodjin-signup==0.1.4      # XXX Still required until we fix settings.py
+
+gunicorn==18.0
+
+# For additional non-python prerequisites see bower.js. Ex:
+# bootstrap 3.3.2
+# fontawesome 4.3.0
+
+django-storages-redux==1.2.3
